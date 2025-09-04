7ebb3984edcfdae58f131b8e23249396015e6085
Marco Guaspari Worms <marcogworms@gmail.com>
2025-09-03 22:24:42 -0300

    feat(contracts): add claim pool reentrancy token

--- test/Reentrancy.test.js
+++ test/Reentrancy.test.js
@@ -3,44 +3,105 @@ const { ethers } = require("hardhat");
 
 describe("Reentrancy protection", function () {
   const ENTRY_FEE = ethers.parseUnits("100", 18);
-  let accounts;
-  let templ;
-  let token;
 
-  beforeEach(async function () {
-    accounts = await ethers.getSigners();
-    const [owner, priest] = accounts;
+  describe("purchaseAccess", function () {
+    let accounts;
+    let templ;
+    let token;
 
-    const ReentrantToken = await ethers.getContractFactory("ReentrantToken");
-    token = await ReentrantToken.deploy("Reentrant Token", "RNT");
-    await token.waitForDeployment();
+    beforeEach(async function () {
+      accounts = await ethers.getSigners();
+      const [owner, priest] = accounts;
 
-    const TEMPL = await ethers.getContractFactory("TEMPL");
-    templ = await TEMPL.deploy(
-      priest.address,
-      priest.address,
-      await token.getAddress(),
-      ENTRY_FEE,
-      10,
-      10
-    );
-    await templ.waitForDeployment();
+      const ReentrantToken = await ethers.getContractFactory("ReentrantToken");
+      token = await ReentrantToken.deploy("Reentrant Token", "RNT");
+      await token.waitForDeployment();
 
-    await token.setTempl(await templ.getAddress());
+      const TEMPL = await ethers.getContractFactory("TEMPL");
+      templ = await TEMPL.deploy(
+        priest.address,
+        priest.address,
+        await token.getAddress(),
+        ENTRY_FEE,
+        10,
+        10
+      );
+      await templ.waitForDeployment();
+
+      await token.setTempl(await templ.getAddress());
+    });
+
+    it("reverts with ReentrantCall when purchaseAccess is reentered", async function () {
+      const attacker = accounts[2];
+
+      await token.mint(attacker.address, ENTRY_FEE);
+      await token
+        .connect(attacker)
+        .approve(await templ.getAddress(), ENTRY_FEE);
+
+      await token.setAttack(true);
+
+      await expect(
+        templ.connect(attacker).purchaseAccess()
+      ).to.be.revertedWithCustomError(templ, "ReentrantCall");
+    });
   });
 
-  it("reverts with ReentrantCall when purchaseAccess is reentered", async function () {
-    const attacker = accounts[2];
+  describe("claimMemberPool", function () {
+    let accounts;
+    let templ;
+    let token;
 
-    await token.mint(attacker.address, ENTRY_FEE);
-    await token.connect(attacker).approve(await templ.getAddress(), ENTRY_FEE);
+    beforeEach(async function () {
+      accounts = await ethers.getSigners();
+      const [owner, priest] = accounts;
 
-    // enable reentrancy attack
-    await token.setAttack(true);
+      const ReentrantClaimToken = await ethers.getContractFactory(
+        "ReentrantClaimToken"
+      );
+      token = await ReentrantClaimToken.deploy(
+        "Reentrant Claim Token",
+        "RCT"
+      );
+      await token.waitForDeployment();
 
-    await expect(
-      templ.connect(attacker).purchaseAccess()
-    ).to.be.revertedWithCustomError(templ, "ReentrantCall");
+      const TEMPL = await ethers.getContractFactory("TEMPL");
+      templ = await TEMPL.deploy(
+        priest.address,
+        priest.address,
+        await token.getAddress(),
+        ENTRY_FEE,
+        10,
+        10
+      );
+      await templ.waitForDeployment();
+
+      await token.setTempl(await templ.getAddress());
+      await token.joinTempl(ENTRY_FEE);
+    });
+
+    it("reverts with ReentrantCall when claimMemberPool is reentered", async function () {
+      const [, member1, member2] = accounts;
+
+      await token.mint(member1.address, ENTRY_FEE);
+      await token.mint(member2.address, ENTRY_FEE);
+
+      await token
+        .connect(member1)
+        .approve(await templ.getAddress(), ENTRY_FEE);
+      await templ.connect(member1).purchaseAccess();
+
+      await token
+        .connect(member2)
+        .approve(await templ.getAddress(), ENTRY_FEE);
+      await templ.connect(member2).purchaseAccess();
+
+      await token.setAttack(true);
+
+      await expect(
+        templ.connect(member1).claimMemberPool()
+      ).to.be.revertedWithCustomError(templ, "ReentrantCall");
+    });
   });
 });
 
