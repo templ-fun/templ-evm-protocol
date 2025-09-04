be437012c542637090a331527b7aac380cb9f1b8
Marco Guaspari Worms <marcogworms@gmail.com>
2025-09-03 22:22:35 -0300

    test(updateConfigDAO): cover EntryFeeTooSmall

--- test/UpdateConfigDAO.test.js
+++ test/UpdateConfigDAO.test.js
@@ -36,6 +36,31 @@ describe("updateConfigDAO", function () {
         await templ.connect(member).purchaseAccess();
     });
 
+    it("reverts when entry fee is less than 10", async function () {
+        const smallFee = 5;
+        const iface = new ethers.Interface([
+            "function updateConfigDAO(address,uint256)"
+        ]);
+        const callData = iface.encodeFunctionData("updateConfigDAO", [
+            ethers.ZeroAddress,
+            smallFee
+        ]);
+
+        await templ.connect(member).createProposal(
+            "Small Fee",
+            "desc",
+            callData,
+            7 * 24 * 60 * 60
+        );
+
+        await templ.connect(member).vote(0, true);
+        await ethers.provider.send("evm_increaseTime", [8 * 24 * 60 * 60]);
+        await ethers.provider.send("evm_mine");
+
+        await expect(templ.executeProposal(0))
+            .to.be.revertedWithCustomError(templ, "EntryFeeTooSmall");
+    });
+
     it("reverts when entry fee is not divisible by 10", async function () {
         const invalidFee = ENTRY_FEE + 5n;
         const iface = new ethers.Interface([
