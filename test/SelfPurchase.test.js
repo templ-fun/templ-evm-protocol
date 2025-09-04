fd7ed28e7af80ce7d4759481065e03f9686f46d0
Marco Guaspari Worms <marcogworms@gmail.com>
2025-09-04 11:48:38 -0300

    test(selfpurchase): ensure direct self-purchase reverts

--- test/SelfPurchase.test.js
+++ test/SelfPurchase.test.js
@@ -67,4 +67,24 @@ describe("Self Purchase Guard", function () {
 
     await ethers.provider.send("hardhat_stopImpersonatingAccount", [templAddress]);
   });
+
+  it("reverts when DAO calls purchaseAccess directly", async function () {
+    const templAddress = await templ.getAddress();
+
+    // fund DAO address with ETH for gas
+    await owner.sendTransaction({
+      to: templAddress,
+      value: ethers.parseEther("1"),
+    });
+
+    // impersonate DAO
+    await ethers.provider.send("hardhat_impersonateAccount", [templAddress]);
+    const dao = await ethers.getSigner(templAddress);
+
+    await expect(
+      templ.connect(dao).purchaseAccess()
+    ).to.be.revertedWithCustomError(templ, "InvalidSender");
+
+    await ethers.provider.send("hardhat_stopImpersonatingAccount", [templAddress]);
+  });
 });
